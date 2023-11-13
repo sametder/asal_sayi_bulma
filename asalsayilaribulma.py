e03513d261e1ddb175b018bbc0d95b52f3cd75da
Samet DER <dersamet4@gmail.com>
2023-11-13 17:24:03 +0300

    AsalSayiBulma projesi bitti.

---
+++ asalsayilaribulma.py
@@ -0,0 +1,14 @@
+#Girilen iki sayının arasındaki asal sayıları bulan program 
+def asalSayilariBul(sayi1, sayi2):
+    for sayi in range(sayi1, sayi2+1):
+        if sayi > 1:
+            for i in range(2, sayi):
+                if (sayi % i == 0):
+                    break
+            else:
+                print(sayi)
+
+sayi1 = int(input('sayi 1:'))
+sayi2 = int(input('sayi 2:'))
+
+asalSayilariBul(sayi1, sayi2)
